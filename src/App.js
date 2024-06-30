d96b844216f66334990c9c5ac650dc02c0a2ea4f
Anthony Coppin <coppinanthony8@gmail.com>
2024-06-30 09:56:36 -0400

    my first react code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+function App(props) {
+  const currDate = new Date ();
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World</h1>
+      <h1> Teron Coppin </h1>
+      <h2> Teron dada is a super star and his name is king Tony </h2>
+      <h2> Today is {currDate.toLocaleDateString()} and the time is {currDate.toLocaleTimeString()}</h2>
     </div>
-  );
+    
+    );
 }
+
 
 export default App;
